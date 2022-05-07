3ee0c1bdaa01055e2d9d6bcbd65cef43031b57f3
Sushant <skp000002@gmail.com>
2022-05-07 11:42:45 +0530

    Minor changes for deployment

--- client/src/Api/intiAPI.js
+++ client/src/Api/intiAPI.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-// let URL;
-// if (process.env.NODE_ENV === "production") {
-//   URL = "https://socially-webapp.herokuapp.com/api/";
-// } else {
-//   URL = "http://localhost:5000/api/";
-// }
+let URL;
+if (process.env.NODE_ENV === "production") {
+  URL = "https://contest-track.herokuapp.com/api/";
+} else {
+  URL = "http://localhost:5000/api/";
+}
 
-let URL = "http://localhost:5000/api/";
 export default axios.create({
   baseURL: URL,
 });
